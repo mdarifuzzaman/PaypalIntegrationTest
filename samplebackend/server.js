2df9c461d75c04cdc59eededf5f0247ddd915e36
Md Arifuzzaman <arif_uap@outlook.com>
2020-09-07 20:17:54 +1000

    add bearer token

--- samplebackend/server.js
+++ samplebackend/server.js
@@ -1,9 +1,11 @@
 
-
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const port = 8000;
 
+
+app.use(bodyParser.json());
 
 const route = function(app){
     app.get('/testendpoint', async (req, res) => {
@@ -13,6 +15,7 @@ const route = function(app){
 }
 
 route(app);
+require('./middleware')(app);
 
 app.listen(port, () => {
     console.log("Server is listening at " + port);
